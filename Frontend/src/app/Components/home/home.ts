1cb0b3085bad801d83490403524141f2d41564b4
{Garga Saha} <{sahagarga000@gmail.com}>
2026-06-20 01:20:34 +0530

    fixed qr feature

--- Frontend/src/app/Components/home/home.ts
+++ Frontend/src/app/Components/home/home.ts
@@ -3,10 +3,11 @@ import { Router } from '@angular/router';
 import { Service } from '../../Services/service';
 import { QRCodeComponent } from 'angularx-qrcode';
 import { Html5QrcodeScanner } from 'html5-qrcode';
+import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [QRCodeComponent],
+  imports: [QRCodeComponent,FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
@@ -18,25 +19,37 @@ export class Home {
   qr:WritableSignal<any>=signal('');
   joinState:WritableSignal<boolean>=signal(false);
   scannedData:WritableSignal<any>=signal('');
+  id="";
   constructor(){
     if(localStorage.getItem("email")==null){
       this.router.navigate(['/login']);
     }
     this.apiService.getCommunityList().subscribe((x:any)=>{
       this.communities.set(x);
-      console.log(this.communities());
+      // console.log(this.communities());
     })
   }
   setQr(id:any){
     this.qrState.set(true);
     this.qr.set(id);
-    console.log(this.qr());
+    // console.log(this.qr());
+  }
+  storeId(){
+    this.scannedData.set(this.id);
+    // console.log(this.scannedData());
   }
   showJoin(){
     this.joinState.set(true);
   }
   stopJoin(){
     this.joinState.set(false);
+  }
+  copyToClipboard(qr:any){
+    navigator.clipboard.writeText(qr).then(() => {
+      console.log('QR code copied to clipboard');
+    }).catch(err => {
+      console.error('Failed to copy to clipboard:', err);
+    });
   }
   startScanner(){
     const scanner = new Html5QrcodeScanner(
@@ -48,10 +61,17 @@ export class Home {
   scanner.render(
     (decodedText) => {
       this.scannedData.set(decodedText);
-      scanner.clear(); // stop scanning
+      this.id=decodedText;
+      scanner.clear();
     },
     () => {}
   );
+  }
+  joinCommunity(){
+    console.log(this.scannedData());
+  }
+  openCommunity(id:any){
+    alert(id);
   }
 }
 
